d8e650b59c38072381107c0e23a00e1a694e75e5
honziktillu <till@spsmb.cz>
2022-09-18 18:44:24 +0200

    Uwu commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.kostickaxddddd {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.kostickaxddddd to javafx.fxml;
+    exports com.example.kostickaxddddd;
+}
